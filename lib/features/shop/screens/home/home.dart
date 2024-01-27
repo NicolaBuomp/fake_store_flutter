4d0a3284938f9f80e08fba227f14db186f629afa
NicolaBuomp <nicolabuompane.dev@gmail.com>
2024-01-27 14:19:36 +0100

    Header container

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:fake_store_flutter/common/widgets/custom_shapes/container/primary_header_container.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +6,14 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TPrimaryHeaderContainer(child: Container()),
+          ],
+        ),
+      ),
+    );
   }
 }
