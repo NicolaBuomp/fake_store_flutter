abe42cbaa88661abab6268cc4e6e426e0ae357c0
NicolaBuomp <nicolabuompane.dev@gmail.com>
2024-02-06 18:04:44 +0100

    configure firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:fake_store_flutter/app.dart';
+import 'package:fake_store_flutter/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const App());
 }
